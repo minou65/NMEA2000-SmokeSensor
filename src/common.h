873a363253e576fa491f6112c024018e2b89b74c
Andy <azogg65@bluewin.ch>
2024-05-13 16:24:18 +0200

    debug_print makros added

--- src/common.h
+++ src/common.h
@@ -9,6 +9,12 @@
 	#include "WProgram.h"
 #endif
 
+extern bool debugMode;
+
+#define DEBUG_PRINT(x) if (debugMode) Serial.print(x) 
+#define DEBUG_PRINTLN(x) if (debugMode) Serial.println(x)
+#define DEBUG_PRINTF(...) if (debugMode) Serial.printf(__VA_ARGS__)
+
 extern uint8_t gN2KSource;
 extern uint8_t gN2KInstance;
 extern uint8_t gN2KSID;
